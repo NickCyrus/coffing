6f4c51b280d1f2b4ad8b5b9142fdf58f991d03ed
NickCyrus <nicklemusduque@gmail.com>
2022-03-03 12:31:55 -0500

    Update coffing.php

--- coffing.php
+++ coffing.php
@@ -1,7 +1,7 @@
 /*
 Plugin Name: Coffing Control
 Plugin URI: https://www.coffing.es/
-Description: Pluins de control de la tienda y productos en general
+Description: Plugin de control de la tienda y productos en general.
 Version: 1.0
 Author: Nick Cyrus Lemus Duque
 Author URI: nicklemusduque@gmail.com
