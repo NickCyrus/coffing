26e4090f8eccd687f062903ff46a066ca69438fa
NickCyrus <nicklemusduque@gmail.com>
2022-03-03 12:16:11 -0500

    Create file plugins

---
+++ coffing.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Coffing Control
+Plugin URI: https://www.coffing.es/
+Description: Pluins de control de la tienda y productos en general
+Version: 1.0
+Author: Nick Cyrus Lemus Duque
+Author URI: nicklemusduque@gmail.com
+License: GPL.2
+*/
